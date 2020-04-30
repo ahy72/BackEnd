5115c0de8d6a82edeca29237fbd466badaaec0a6
y-ohno-msl <falos.chaos@gmail.com>
2020-04-30 18:54:41 +0900

    add azure url

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace BackEnd
                 options.AddPolicy(name: CorsPolicyName,
                     builder =>
                     {
-                        builder.WithOrigins("http://127.0.0.1:8081").WithMethods("GET");
+                        builder.WithOrigins("http://127.0.0.1:8081","https://virtualmachinestatus.azurewebsites.net/").WithMethods("GET");
                     });
             });
 
